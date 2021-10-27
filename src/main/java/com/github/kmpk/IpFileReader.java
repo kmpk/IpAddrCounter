f9243a0376568753edfb75d7c1bc968d990bc482
kmpk <kmpk@gmail.com>
2021-10-27 22:44:55 +0300

    Switch from standard Files::lines to manual parallel file reading using SeekableByteChannel by every thread

--- src/main/java/com/github/kmpk/IpFileReader.java
+++ src/main/java/com/github/kmpk/IpFileReader.java
@@ -1,26 +1,85 @@
 package com.github.kmpk;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.nio.file.StandardOpenOption;
+import java.util.function.Consumer;
 
-public class IpFileReader implements AutoCloseable {
-    private final IntStream stream;
+public class IpFileReader implements Runnable {
+    private final Path file;
+    private final long initPos;
+    private final long toPos;
+    private final int byteBuffer;
+    private final IpCounter counter;
+    private final Consumer<Exception> exceptionHandler;
+    private final IpConverter converter = new IpConverter();
 
-    public IpFileReader(Path file, IpConverter ipConverter) throws IOException {
-        Stream<String> lines = Files.lines(file, StandardCharsets.US_ASCII);
-        this.stream = lines.parallel().mapToInt(ipConverter::convertIpv4ToInt);
+    public IpFileReader(Path file, long initPos, long toPos, int bufferSize, IpCounter counter, Consumer<Exception> exceptionHandler) {
+        this.file = file;
+        this.initPos = initPos;
+        this.toPos = toPos;
+        this.byteBuffer = bufferSize;
+        this.counter = counter;
+        this.exceptionHandler = exceptionHandler;
     }
 
-    public IntStream getIpsIntStream() {
-        return stream;
+    private void readIpsAndAddToCounter() throws IOException {
+        ByteBuffer bf = ByteBuffer.allocate(byteBuffer);
+        try (SeekableByteChannel seekableByteChannel = Files.newByteChannel(file, StandardOpenOption.READ)) {
+            seekableByteChannel.position(initPos);
+            findNextLineAndSetPosition(seekableByteChannel, bf);
+            process(bf, seekableByteChannel);
+        }
+    }
+
+    private void process(ByteBuffer bf, SeekableByteChannel seekableByteChannel) throws IOException {
+        StringBuilder current = new StringBuilder();
+        long currentPosition = seekableByteChannel.position();
+        long fileSize = seekableByteChannel.size();
+        while (currentPosition < fileSize && currentPosition < toPos) {
+            seekableByteChannel.read(bf);
+            bf.flip();
+            while (bf.position() < bf.limit()) {
+                char c = (char) bf.get();
+                if (c == '\n') {
+                    counter.add(converter.convertIpv4ToInt(current));
+                    current.setLength(0);
+                } else {
+                    current.append(c);
+                }
+            }
+            bf.clear();
+            currentPosition = seekableByteChannel.position();
+        }
+        if (currentPosition == fileSize) {
+            counter.add(converter.convertIpv4ToInt(current));
+        }
+    }
+
+    private void findNextLineAndSetPosition(SeekableByteChannel seekableByteChannel, ByteBuffer bf) throws IOException {
+        if (initPos != 0) {
+            seekableByteChannel.read(bf);
+            bf.flip();
+            int c = 1;
+            while (bf.position() < bf.limit()) {
+                char c1 = (char) bf.get();
+                if (c1 == '\n') break;
+                c++;
+            }
+            seekableByteChannel.position(initPos + c);
+            bf.clear();
+        }
     }
 
     @Override
-    public void close() {
-        stream.close();
+    public void run() {
+        try {
+            readIpsAndAddToCounter();
+        } catch (Exception e) {
+            exceptionHandler.accept(e);
+        }
     }
 }
--- src/main/java/com/github/kmpk/Main.java
+++ src/main/java/com/github/kmpk/Main.java
@@ -1,21 +1,50 @@
 package com.github.kmpk;
 
-import java.io.IOException;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
-    public static void main(String[] args) {
+    public static final int BYTE_BUFFER_SIZE = 1024 * 1024; //1MB
+    public static final int BLOCK_OVERLAP_BYTES = 20;
+
+    public static void main(String[] args) throws InterruptedException {
         if (args.length != 1) {
             System.out.println("Pass file path as command-line argument, example: \"/path/to/file\"");
             System.exit(0);
         }
+        IpCounter counter = new IpCounter();
+        Path file = Path.of(args[0]);
 
-        try (IpFileReader reader = new IpFileReader(Path.of(args[0]), new IpConverter())) {
-            IpCounter counter = new IpCounter();
-            reader.getIpsIntStream().forEach(counter::add);
-            System.out.println(counter.currentCount());
-        } catch (IOException e) {
-            e.printStackTrace();
+        long fileLength = file.toFile().length();
+        long block = calculateBlockSize(fileLength);
+
+        ExecutorService service = ForkJoinPool.commonPool();
+        AtomicReference<Exception> ex = new AtomicReference<>();
+
+        long leftBound = 0;
+        while (leftBound < fileLength) {
+            long rightBound = Math.min(leftBound + block + BLOCK_OVERLAP_BYTES, fileLength);
+            service.execute(new IpFileReader(file, leftBound, rightBound, BYTE_BUFFER_SIZE, counter, e -> {
+                ex.compareAndSet(null, e);
+                service.shutdownNow();
+            }));
+            leftBound = rightBound == fileLength ? rightBound : rightBound - BLOCK_OVERLAP_BYTES;
         }
+
+        service.shutdown();
+        service.awaitTermination(1, TimeUnit.HOURS);
+        if (ex.get() != null) {
+            ex.get().printStackTrace();
+        } else {
+            System.out.println(counter.currentCount());
+        }
+    }
+
+    private static long calculateBlockSize(long fileLength) {
+        int maxThreads = Runtime.getRuntime().availableProcessors();
+        return Math.max(fileLength / maxThreads, BYTE_BUFFER_SIZE);
     }
 }
