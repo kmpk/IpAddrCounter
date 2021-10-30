53f039e80ac6af32eee9da9c49942f65ee1357c0
kmpk <kmpk.gm@gmail.com>
2021-10-30 15:51:00 +0300

    Add Native Image support

--- src/main/java/com/github/kmpk/Main.java
+++ src/main/java/com/github/kmpk/Main.java
@@ -1,8 +1,12 @@
 package com.github.kmpk;
 
+import org.graalvm.nativeimage.ImageInfo;
+
 import java.nio.file.Path;
 
 public class Main {
+    private static final String HELP_FORMAT = "Pass file path as command-line argument, example: %s \"/path/to your/file\"";
+
     private static Path path;
 
     public static void main(String[] args) {
@@ -20,9 +24,19 @@ public class Main {
     }
 
     private static void printHelpAndExit() {
-        System.out.println("""
-                Pass file path as command-line argument, example: java -jar IpAddrCounter.jar "/path/to your/file"
-                """);
+        String programCommand = "java -jar IpAddrCounter.jar";
+        try {
+            Class.forName("org.graalvm.nativeimage.ImageInfo");
+            if (ImageInfo.inImageRuntimeCode()) {
+                if (System.getProperty("os.name").contains("Windows")) {
+                    programCommand = "IpAddrCounter.exe";
+                } else {
+                    programCommand = "IpAddrCounter";
+                }
+            }
+        } catch (ClassNotFoundException ignore) {
+        }
+        System.out.printf(HELP_FORMAT, programCommand);
         System.exit(0);
     }
 }
